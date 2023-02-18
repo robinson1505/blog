574d3aac4eb851aabe0721fb81deaf031ecd35e7
robinson1505 <robinsonanthony1505@gmail.com>
2023-02-18 15:28:52 +0300

    create a react app and setup the navigation bar

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+import './App.css';
+
+function App() {
+  return (
+   <main>
+    <header>
+      <a href= "" className="logo">My Logo</a>
+      <nav>
+      <a href ="">Login</a>
+      <a href="">Register</a>
+   
+
+    </nav>
+    </header>
+   
+   </main>
+  );
+}
+
+export default App;
