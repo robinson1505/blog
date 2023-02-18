35c5c0ce7aa54d0dd8ac1e99913ce2451af54ac5
robinson1505 <robinsonanthony1505@gmail.com>
2023-02-18 16:16:47 +0300

    create posts using react

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,75 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-   <main>
-    <header>
-      <a href= "" className="logo">My Logo</a>
-      <nav>
-      <a href ="">Login</a>
-      <a href="">Register</a>
-   
+    <main>
+      <header>
+        <a href="" className="logo">
+          My Logo
+        </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+          <img src="https://ngoswebrand.com/wp-content/uploads/2022/06/COLLAGE-2.jpg" />
+        </div>
 
-    </nav>
-    </header>
-   
-   </main>
+        <div className="text">
+        <h2>Business Colleges in Tanzania</h2>
+          <p className="info">
+            <a href ="" className="author"> Robin Anthony</a>
+            <time>2023-02-18 16:00</time>
+          </p>
+          <p className="summary">
+            Different university of Tanzania provide kozi za biashara to
+            different student according to the vyuo vya biashara which are
+            available in Tanzania.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://ngoswebrand.com/wp-content/uploads/2022/06/COLLAGE-2.jpg" />
+        </div>
+
+        <div className="text">
+          <h2>Business Colleges in Tanzania</h2>
+          <p className="info">
+            <a href ="" className="author"> Robin Anthony</a>
+            <time>2023-02-18 16:00</time>
+          </p>
+          <p className="summary">
+            Different university of Tanzania provide kozi za biashara to
+            different student according to the vyuo vya biashara which are
+            available in Tanzania.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://ngoswebrand.com/wp-content/uploads/2022/06/COLLAGE-2.jpg" />
+        </div>
+
+        <div className="text">
+        <h2>Business Colleges in Tanzania</h2>
+          <p className="info">
+            <a href ="" className="author"> Robin Anthony</a>
+            <time>2023-02-18 16:00</time>
+          </p>
+          <p className="summary">
+            Different university of Tanzania provide kozi za biashara to
+            different student according to the vyuo vya biashara which are
+            available in Tanzania.
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
